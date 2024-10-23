15f9cc57eff24705adf47ec6f85706fa444c3b65
Manuel Tello Siri <manuelTello.business@gmail.com>
2024-10-23 13:46:33 -0700

    Account delete response id field changed to string

--- VeterinarySlices.API/Features/Accounts/DeleteAccount/DeleteAccountCommandResponse.cs
+++ VeterinarySlices.API/Features/Accounts/DeleteAccount/DeleteAccountCommandResponse.cs
@@ -5,6 +5,6 @@ namespace VeterinarySlices.API.Features.Accounts.DeleteAccount
     public class DeleteAccountCommandResponse
     {
         [JsonPropertyName("removed_id")]
-        public Guid RemovedId { get; set; }
+        public string RemovedId { get; set; } = string.Empty;
     }
 }
